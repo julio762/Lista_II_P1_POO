78d14be50c061943de59d1cce3f8097ee0198e37
julio762 <julio.almeida.jr3009@gmail.com>
2026-05-29 13:36:40 -0400

    Update funcionario.py

--- 2-Sistema_Funcionários_Empresa/models/funcionario.py
+++ 2-Sistema_Funcionários_Empresa/models/funcionario.py
@@ -1,1 +1,14 @@
 
+from abc import ABC, abstractmethod
+
+class Funcionario(ABC):
+    def __init__(self, nome: str, cpf: str):
+        self.nome = nome
+        self.cpf = cpf
+
+    def mostrar_dados(self):
+        print(f"Nome: {self.nome} | CPF: {self.cpf}")
+
+    @abstractmethod
+    def calcular_pagamento(self) -> float:
+        pass
