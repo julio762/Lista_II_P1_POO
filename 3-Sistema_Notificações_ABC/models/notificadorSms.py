40607c6a075cd2d9f3f0d39e7bb5b2c495538b65
julio762 <julio.almeida.jr3009@gmail.com>
2026-05-29 13:48:36 -0400

    Add NotificadorSMS class for SMS notifications

--- 3-Sistema_Notificações_ABC/models/notificadorSms.py
+++ 3-Sistema_Notificações_ABC/models/notificadorSms.py
@@ -1,1 +1,6 @@
 
+from notificador import Notificador
+
+class NotificadorSMS(Notificador):
+    def notificar(self, mensagem: str):
+        print(f"[SMS] Enviando torpedo para celular: '{mensagem}'")
