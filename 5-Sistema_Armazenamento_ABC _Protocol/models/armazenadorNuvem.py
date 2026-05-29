59366a76a6ae8ab016ccd7407538fa62255e4f1f
julio762 <julio.almeida.jr3009@gmail.com>
2026-05-29 14:12:18 -0400

    Update armazenadorNuvem.py

--- 5-Sistema_Armazenamento_ABC _Protocol/models/armazenadorNuvem.py
+++ 5-Sistema_Armazenamento_ABC _Protocol/models/armazenadorNuvem.py
@@ -1,1 +1,6 @@
+from typing import Any
 
+class ArmazenadorNuvem:
+    # Esta classe não herda explicitamente da ABC Armazenador
+    def salvar(self, dado: Any):
+        print(f"[Protocol - Nuvem] Enviando payload via API para servidor remoto: {dado}")
