c13361736632cac5288b63808775259d8607e0f9
julio762 <julio.almeida.jr3009@gmail.com>
2026-05-29 13:49:13 -0400

    Add NotificadorApp class for push notifications

--- 3-Sistema_Notificações_ABC/models/notificadorApp.py
+++ 3-Sistema_Notificações_ABC/models/notificadorApp.py
@@ -1,1 +1,6 @@
 
+from notificador import Notificador
+
+class NotificadorApp(Notificador):
+    def notificar(self, mensagem: str):
+        print(f"[Push Notification] Exibindo na tela do smartphone: '{mensagem}'")
