b862980ae02b11552f9e62a60f16ba159c350cad
julio762 <julio.almeida.jr3009@gmail.com>
2026-05-29 13:25:56 -0400

    Add Video class extending Midia with playback method

--- 1-Sistema_midias_educacionais/models/video.py
+++ 1-Sistema_midias_educacionais/models/video.py
@@ -1,1 +1,10 @@
 
+from midia import Midia
+
+class Video(Midia):
+    def __init__(self, titulo: str, duracao: int, resolucao: str):
+        super().__init__(titulo, duracao)
+        self.resolucao = resolucao 
+
+    def reproduzir(self): 
+        print(f"[Vídeo] Reproduzindo '{self.titulo}' na resolução {self.resolucao}")
