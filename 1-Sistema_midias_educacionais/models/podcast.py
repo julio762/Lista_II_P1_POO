225aed537eb91ff5422dc2cf9f84aa4885294cb7
julio762 <julio.almeida.jr3009@gmail.com>
2026-05-29 13:23:49 -0400

    Add Podcast class extending Midia

--- 1-Sistema_midias_educacionais/models/podcast.py
+++ 1-Sistema_midias_educacionais/models/podcast.py
@@ -1,1 +1,10 @@
 
+from midia import Midia
+
+class Podcast(Midia):
+    def __init__(self, titulo: str, duracao: int, apresentador: str):
+        super().__init__(titulo, duracao)
+        self.apresentador = apresentador
+
+    def reproduzir(self):
+        print(f"[Podcast] Ouvindo '{self.titulo}', com o apresentador {self.apresentador}")
