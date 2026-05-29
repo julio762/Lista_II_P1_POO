5db502df125ef057c848e994ea39da59f4eca21a
julio762 <julio.almeida.jr3009@gmail.com>
2026-05-29 13:45:00 -0400

    Add NotificadorEmail class for email notifications

--- 3-Sistema_Notificações_ABC/models/notificadorEmail.py
+++ 3-Sistema_Notificações_ABC/models/notificadorEmail.py
@@ -1,1 +1,6 @@
 
+from notificador import Notificador
+
+class NotificadorEmail(Notificador):
+    def notificar(self, mensagem: str):
+        print(f"[E-mail] Enviando para caixa de entrada: '{mensagem}'")
