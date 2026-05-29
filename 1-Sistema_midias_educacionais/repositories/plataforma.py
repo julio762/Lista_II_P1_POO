1c9010c707ba5128fba949d59c380838f9fdb3f0
julio762 <julio.almeida.jr3009@gmail.com>
2026-05-29 13:29:08 -0400

    Update plataforma.py

--- 1-Sistema_midias_educacionais/repositories/plataforma.py
+++ 1-Sistema_midias_educacionais/repositories/plataforma.py
@@ -1,1 +1,20 @@
 
+from midia import Midia
+
+class Plataforma:
+    def __init__(self, nome: str):
+        self.nome = nome
+        self.lista_midias = []
+
+    def adicionar_midia(self, midia: Midia):
+        self.lista_midias.append(midia)
+
+    def listar_midias(self):
+        print(f"\nMídias Disponíveis na Plataforma {self.nome}")
+        for midia in self.lista_midias:
+            midia.mostrar_info()
+
+    def reproduzir_todas(self):
+        print(f"\nReprodução ({self.nome})")
+        for midia in self.lista_midias:
+            midia.reproduzir()
