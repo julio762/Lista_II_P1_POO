b7874cd7b370a58491fad7d1576872e99057f1af
julio762 <julio.almeida.jr3009@gmail.com>
2026-05-29 13:40:12 -0400

    Implement Empresa class with employee management methods

--- 2-Sistema_Funcionários_Empresa/repositories/empresa.py
+++ 2-Sistema_Funcionários_Empresa/repositories/empresa.py
@@ -1,1 +1,24 @@
 
+from funcionario import Funcionario
+
+class Empresa:
+    def __init__(self, nome: str):
+        self.nome = nome
+        self.lista_funcionarios = []
+
+    def adicionar_funcionario(self, funcionario: Funcionario):
+        self.lista_funcionarios.append(funcionario)
+
+    def listar_funcionarios(self):
+        print(f"\n--- Cadastro Geral de Funcionários da {self.nome} ---")
+        for f in self.lista_funcionarios:
+            f.mostrar_dados()
+
+    def mostrar_folha_pagamento(self):
+        print(f"\n--- Folha de Pagamento - {self.nome} ---")
+        total_folha = 0.0
+        for f in self.lista_funcionarios:
+            pagamento = f.calcular_pagamento()
+            print(f"Funcionário: {f.nome} | Valor Devido: R$ {pagamento:.2f}")
+            total_folha += pagamento
+        print(f"--> Total da Folha: R$ {total_folha:.2f}")
