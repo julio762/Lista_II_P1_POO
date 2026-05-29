63e5a9a5ef7c647622df802afb4060968be1ac41
julio762 <julio.almeida.jr3009@gmail.com>
2026-05-29 13:54:11 -0400

    Add CentralNotificacoes class for message broadcasting

--- 3-Sistema_Notificações_ABC/repositories/centralNotificações.py
+++ 3-Sistema_Notificações_ABC/repositories/centralNotificações.py
@@ -1,1 +1,14 @@
 
+from notificador import Notificador
+
+class CentralNotificacoes:
+    def __init__(self):
+        self.notificadores = []
+
+    def adicionar_notificador(self, notificador: Notificador):
+        self.notificadores.append(notificador)
+
+    def enviar_para_todos(self, mensagem: str):
+        print(f"\n--- Transmitindo Mensagem Centralizada ---")
+        for n in self.notificadores:
+            n.notificar(mensagem)
