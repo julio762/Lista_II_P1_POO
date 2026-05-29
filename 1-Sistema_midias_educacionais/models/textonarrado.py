3f76d1d94de8fa6cb5cb6f4a3b9e05e697501df0
julio762 <julio.almeida.jr3009@gmail.com>
2026-05-29 13:25:25 -0400

    Add TextoNarrado class for narrated text media

--- 1-Sistema_midias_educacionais/models/textonarrado.py
+++ 1-Sistema_midias_educacionais/models/textonarrado.py
@@ -1,1 +1,10 @@
 
+from midia import Midia
+
+class TextoNarrado(Midia):
+    def __init__(self, titulo: str, duracao: int, idioma: str):
+        super().__init__(titulo, duracao)
+        self.idioma = idioma
+
+    def reproduzir(self):
+        print(f"[Texto Narrado] Escutando a narração de '{self.titulo}' em {self.idioma}")
